11dcd7b4f929e52d802a7502a7108ee699110d48
Jose Lopez <jose.lopez@sendgrid.com>
2014-07-13 12:43:24 -0700

    add centos dependencies in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 
-# install rvm and add source
+#
+# http://askubuntu.com/questions/459402/how-to-know-if-the-running-platform-is-ubuntu-or-centos-with-help-of-a-bash-scri
+# http://geekbraindump.blogspot.com/2010/06/case-insensitive-regex-in-bash.html
 
+#
+# Determine OS platform
+UNAME=$(uname | tr "[:upper:]" "[:lower:]")
+# If Linux, try to determine specific distribution
+if [ "$UNAME" == "linux" ]; then
+    # If available, use LSB to identify distribution
+    if [ -f /etc/lsb-release -o -d /etc/lsb-release.d ]; then
+        export DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
+    # Otherwise, use release info file
+    else
+        export DISTRO=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1)
+    fi
+fi
+
+# setup ubuntu dependencies
+shopt -s nocasematch
+if [[ "$DISTRO" =~ "ubuntu" ]]; then
+  continue
+
+# setup centOS dependencies
+elif [[ "$DISTRO" =~ "centos" ]]
+  sudo yum install -y libyaml-devel
+
+# os not supported yet
+else
+  echo 'operating system not supported'
+  exit 1
+fi
+
+# install rvm and add source
 \curl -L https://get.rvm.io | bash -s stable --ruby
 source ~/.rvm/scripts/rvm
 RVM_SOURCE=$(cat ~/.bashrc | grep '.rvm/scripts/rvm' | wc -l)
